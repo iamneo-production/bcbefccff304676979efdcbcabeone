d9dcf0d5936b74fe797a9734b130a481d05194d9
Iamneo <techadmin@iamneo.ai>
2023-09-03 12:50:12 +0000

    Backup Commit

--- public/app.js
+++ public/app.js
@@ -1,10 +1,5 @@
 // Initial game state
-let cells = ['', '', '', '', '', '', '', '', ''];
-let currentPlayer = 'X';
-let result = document.querySelector('.result');
-let btns = document.querySelectorAll('.btn');
-let conditions = [
-    [0, 1, 2],
+
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
